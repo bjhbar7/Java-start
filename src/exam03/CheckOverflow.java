659c3c51879c7c0ce55ca9e30034932556f67495
bae jin ryang <bjhbar7@gmail.com>
2021-12-27 22:20:36 +0900

    21.12.27 Exam02 point) safeAdd()

---
+++ src/exam03/CheckOverflow.java
@@ -0,0 +1,28 @@
+package exam03;
+
+public class CheckOverflow {
+public static void main(String[] args) {
+	try {
+		int result = safeAdd(2000000000,2000000000);
+		System.out.println(result);
+	} catch(ArithmeticException e) {
+		System.out.println("오버플로우가 발생하여 정확하게 계산할 수 없음");
+	} // 예외 처리 코드
+}
+
+public static int safeAdd(int left, int right) {
+	if((right>0)) {
+		if(left>(Integer.MAX_VALUE - right))
+		// left + right > Integer.MAX_VALUE 식 변형
+			{throw new ArithmeticException("오버플로우 발생");
+			//예외 발생 코드			
+		}
+	} else // right<=0일 경우
+	{if(left<(Integer.MIN_VALUE - right)) {
+		throw new ArithmeticException("오버플로우 발생");
+		//예외 발생 코드
+	}
+}
+	return left + right;
+}
+}
