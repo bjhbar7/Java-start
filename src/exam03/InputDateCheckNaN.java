192c4969e823bdd635e6f28aa4ec3ee14e05c59a
bae jin ryang <bjhbar7@gmail.com>
2021-12-28 18:33:47 +0900

    21.12.28 Exam01

--- src/exam03/InputDateCheckNaN.java
+++ src/exam03/InputDateCheckNaN.java
@@ -1,5 +1,18 @@
 package exam03;
 
 public class InputDateCheckNaN {
-
+public static void main(String[] args) {
+	String userInput = "NaN";
+	double val = Double.valueOf( userInput ); //입력값을 double 타입으로 변환
+	
+	double currentBalance = 10000.0;
+	
+	if(Double.isNaN(val)) {
+		System.out.println("NaN이 입력되어 처리할 수 없음");
+		val= 0.0;
+	} // NaN을 검사하여 , NaN일 경우 실행되는 코드
+	
+	currentBalance += val;
+	System.out.println(currentBalance);
+}
 }
