3f848ed2ea2b84f9e061b173e3a36c2950053d38
bae jin ryang <bjhbar7@gmail.com>
2022-01-15 22:35:58 +0900

    22.01.15 Exam

---
+++ src/exam06/Calculator03.java
@@ -0,0 +1,13 @@
+package exam06;
+
+public class Calculator03 {
+//정사각형의 넓이
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	//직사각형의 넓이
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+}
