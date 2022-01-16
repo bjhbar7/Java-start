1cb75730d745d72c861c88cfb9ef34c6b55b83f3
bae jin ryang <bjhbar7@gmail.com>
2022-01-16 22:33:16 +0900

    22.01.16 Exam

---
+++ src/exam06/CarExam02.java
@@ -0,0 +1,11 @@
+package exam06;
+
+public class CarExam02 {
+public static void main(String[] args) {
+	Car07 myCar = new Car07("Æ÷¸£½¦");
+	Car07 yourCar = new Car07("º¥Ã÷");
+	
+	myCar.run();
+	yourCar.run();
+}
+}
