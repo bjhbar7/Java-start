b2c667638098a08adbdcf6f63c65a03b9fbe93e6
bae jin ryang <bjhbar7@gmail.com>
2022-01-16 23:02:38 +0900

    22.01.16 Exam02

---
+++ src/exam06/CalculatorExam04.java
@@ -0,0 +1,13 @@
+package exam06;
+
+public class CalculatorExam04 {
+public static void main(String[] args) {
+	double result1 = 10*10*Calculator04.pi;
+	int result2 = Calculator04.plus(10, 5);
+	int result3 = Calculator04.minus(10, 5);
+	
+	System.out.println("result1 :" +result1);
+	System.out.println("result2 :" +result2);
+	System.out.println("result3 :" +result3);
+}
+}
