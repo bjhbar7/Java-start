c3f4fc9a8ac56b66a1ba5d852621d3f76473b854
bjhbar7 <bjhbar7@gmail.com>
2022-02-12 23:58:26 +0900

    220212 Exam02

--- src/exam07_third/Student.java
+++ src/exam07_third/Student.java
@@ -1,5 +1,7 @@
 package exam07_third;
 
+import java.util.ArrayList;
+
 public class Student {
 	int studentID;
 	String studentName;
@@ -8,20 +10,20 @@ public class Student {
 	public Student(int studentID, String studentName) {
 		this.studentID = studentID;
 		this.studentName = studentName;
-		subjectList =new ArrayList<Subject> ();
+		subjectLest =new ArrayList<Subject> ();
 	}
 	
 	public void addSubject(String name, int score) {
 		Subject subject = new Subject();
 		subject.setName(name);
 		subject.setScorePoint(score);
-		subjectList.add(subject);
+		subjectLest.add(subject);
 	}
 	
 	public void showStudentInfo() {
-		int totatl = 0;
-		for(Subject s : subjectList) {
-			totatl += s.getScorePoint();
+		int total = 0;
+		for(Subject s : subjectLest) {
+			total += s.getScorePoint();
 			System.out.println("학생 " + studentName + "의 " + s.getName() + "과목성적은" + s.getScorePoint() + "입니다.");
 		}
 	}
